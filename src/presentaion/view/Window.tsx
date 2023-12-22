3e8c76fc4fbf320113ea34f798ccd73346cb130d
김솔지 <so1gging@gmail.com>
2023-12-22 22:59:10 +0900

    #4 Snowly 컴포넌트로 교체

--- src/presentaion/view/Window.tsx
+++ src/presentaion/view/Window.tsx
@@ -1,23 +1,9 @@
-import { useEffect } from 'react'
-import { Snowly } from '../../utils/snow.ts'
+import Snowly from '../components/Snowly/Snowly.tsx'
 
 export default function Window() {
-  useEffect(() => {
-    const canvas = document.getElementById('test') as HTMLCanvasElement
-    const ctx = canvas.getContext('2d')!
-
-    const snow = Snowly(canvas, ctx)
-
-    const ss = snow.init()
-
-    window.onresize = () => {
-      snow.canvasWidth = canvas.clientWidth
-      snow.canvasHeight = canvas.clientHeight
-
-      snow.make()
-    }
-    return () => cancelAnimationFrame(ss)
-  }, [])
-
-  return <canvas id="test" width="500px" height="500px" style={{ backgroundColor: 'blue' }}></canvas>
+  return (
+    <Snowly width={500} height={500}>
+      <Snowly.Flake />
+    </Snowly>
+  )
 }
