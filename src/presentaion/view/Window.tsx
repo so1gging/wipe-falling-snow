83d4f8bd2d6f236af010006a3dcf77ba5374254f
김솔지 <so1gging@gmail.com>
2023-12-22 21:50:48 +0900

    #3 prototype snow util 추가

--- src/presentaion/view/Window.tsx
+++ src/presentaion/view/Window.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from 'react'
+import { Snowly } from '../../utils/snow.ts'
+
 export default function Window() {
-  return <>Window</>
+  useEffect(() => {
+    const canvas = document.getElementById('test') as HTMLCanvasElement
+    const ctx = canvas.getContext('2d')!
+
+    const snow = Snowly(canvas, ctx)
+
+    const ss = snow.init()
+
+    window.onresize = () => {
+      snow.canvasWidth = canvas.clientWidth
+      snow.canvasHeight = canvas.clientHeight
+
+      snow.make()
+    }
+    return () => cancelAnimationFrame(ss)
+  }, [])
+
+  return <canvas id="test" width="500px" height="500px" style={{ backgroundColor: 'blue' }}></canvas>
 }
