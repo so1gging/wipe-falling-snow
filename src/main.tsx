ded590b7d3b726774c86a53c982ae57a7f4dfd4d
김솔지 <so1gging@gmail.com>
2023-12-23 20:00:17 +0900

    #9 기본 레이아웃 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from '@/AppRouter.tsx'
+import Layout from '@/Layout.tsx'
+import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Layout>
   </React.StrictMode>,
 )
