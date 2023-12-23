3a8ed69c873af33733e30e13c1d8c8ac8f9e548f
김솔지 <so1gging@gmail.com>
2023-12-23 17:18:26 +0900

    #5 랜덤 정수를 리턴하는 랜덤함수 추가

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,7 @@
 export const getRandomWithinBounds = (min: number, max: number) => {
   return Math.random() * (max - min) + min
 }
+
+export const getRandomIntegerWithinBounds = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) + min)
+}
