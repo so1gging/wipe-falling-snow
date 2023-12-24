e516325fbeb56a55e3a7da79e43915aa2c361626
김솔지 <so1gging@gmail.com>
2023-12-24 14:32:29 +0900

    #13 마우스 포인터 값을 받을 수 있는 MouseTriggerContext 추가

---
+++ src/context/MouseTriggerContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, PropsWithChildren, useEffect, useRef, useState } from 'react'
+
+interface MouseTriggerContextType {
+  x: number
+  y: number
+}
+export const MouseTriggerContext = createContext<MouseTriggerContextType>({ x: 0, y: 0 })
+export default function MouseTrigger({ children }: PropsWithChildren) {
+  const triggerRef = useRef<HTMLDivElement | null>(null)
+  const [state, setState] = useState<MouseTriggerContextType>({ x: 0, y: 0 })
+
+  useEffect(() => {
+    if (!triggerRef || !triggerRef.current) {
+      return
+    }
+    const mousemove: EventListener = (e: MouseEventInit) => {
+      const { clientX, clientY } = e
+      if (clientX && clientY) {
+        setState({ x: clientX, y: clientY })
+      }
+    }
+
+    triggerRef.current.addEventListener('mousemove', mousemove)
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => triggerRef.current?.removeEventListener('mousemove', mousemove)
+  })
+
+  return (
+    <MouseTriggerContext.Provider value={state}>
+      <div ref={triggerRef}>{children}</div>
+    </MouseTriggerContext.Provider>
+  )
+}
