b394de52f7fa1ac2ed3f00e004870ce5a9c0fd08
김솔지 <so1gging@gmail.com>
2023-12-24 22:17:26 +0900

    #14 index 화면 적용

--- src/presentaion/view/Home.tsx
+++ src/presentaion/view/Home.tsx
@@ -1,9 +1,35 @@
 import { Link } from 'react-router-dom'
+import { ROOT_SIZE } from '@/utils/const.ts'
 
 export default function Home() {
   return (
-    <div>
-      <Link to="/window">Window</Link>
+    <div
+      style={{
+        width: '100%',
+        height: `${ROOT_SIZE}px`,
+        backgroundImage: 'url("/main-background.png")',
+        backgroundSize: 'contain',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          paddingTop: '20px',
+        }}
+      >
+        <div
+          style={{
+            backgroundImage: 'url("/logo.png")',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            width: '200px',
+            height: '70px',
+          }}
+        />
+      </div>
+      <Link to="/window">START</Link>
     </div>
   )
 }
