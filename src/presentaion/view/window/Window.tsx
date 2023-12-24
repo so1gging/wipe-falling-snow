104dee681a52c963aca382dcacd17f0054e3746b
김솔지 <so1gging@gmail.com>
2023-12-24 22:23:05 +0900

    #14 화면이 고정되어 있으므로, 굳이 윈도우가 resize될 때 다시그릴 필요가 없음.

--- src/presentaion/view/window/Window.tsx
+++ src/presentaion/view/window/Window.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { WrapperContext } from '@/context/WrapperContext.tsx'
 import Frost from '@/presentaion/domains/Frost/Frost.tsx'
 import SnowCanvas from '@/presentaion/domains/SnowCanvas/SnowCanvas.tsx'
@@ -7,26 +7,12 @@ import { FROST_UNIT_WIDTH } from '@/utils/const.ts'
 
 export default function Window() {
   const wrapperContext = useContext(WrapperContext)
-  const [screen, setScreen] = useState({
+  const screen = {
     width: wrapperContext.element.offsetWidth,
     height: wrapperContext.element.offsetHeight,
-  })
+  }
   const xSize = Math.floor(screen.width / FROST_UNIT_WIDTH + 1)
   const ySize = Math.floor(screen.height / FROST_UNIT_WIDTH)
-
-  useEffect(() => {
-    const resize = () => {
-      if (
-        wrapperContext.element.offsetWidth !== screen.width ||
-        wrapperContext.element.offsetHeight !== screen.height
-      ) {
-        setScreen({ width: wrapperContext.element.offsetWidth, height: wrapperContext.element.offsetHeight })
-      }
-    }
-    window.addEventListener('resize', resize)
-
-    return () => window.removeEventListener('resize', resize)
-  })
 
   return (
     <MouseTrigger>
