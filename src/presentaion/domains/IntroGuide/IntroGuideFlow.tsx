c848d760e7eafa44dbe29e1502bb5fa2c02092e7
김솔지 <so1gging@gmail.com>
2023-12-27 18:47:05 +0900

    #19 외부에서 props를 넘기도록

--- src/presentaion/domains/IntroGuide/IntroGuideFlow.tsx
+++ src/presentaion/domains/IntroGuide/IntroGuideFlow.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
 import Dialog from '@/presentaion/components/Dialog/Dialog.tsx'
 
-export default function IntroGuideFlow() {
-  const [step, setStep] = useState(0)
-  const flow = [
-    '안녕? 나는 루피야.\n보시다시피 눈사람이지.',
-    '내 친구 쵸파가\n나를 만들어줬어...',
-    '쵸파와 난,\n둘도 없는 친구가 되었지...',
-    '난 늘 같은 시간 찾아오는\n쵸파를 기다려...',
-    '매일 같은 시간, 쵸파는 늘\n나와 같이 놀아주거든...',
-    '오늘도 쵸파가 와줄거야...',
-    '나랑 같이 쵸파를 기다려줄래?',
-  ]
-
+interface IntroGuideFlowProp {
+  step: number
+  flow: string[]
+  onIncrease: () => void
+}
+export default function IntroGuideFlow({ step, flow, onIncrease }: IntroGuideFlowProp) {
   const handleNextStep = () => {
-    if (step !== flow.length - 1) {
-      setStep((prev) => prev + 1)
-    }
+    onIncrease()
   }
 
   return (
